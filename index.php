c3d944e6ee8916e4d84c6a7a9ac85d094f87af63
pranavpreet <37465828+pranavpreet@users.noreply.github.com>
2018-03-17 16:57:26 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<html>
+<body background="image/college.jpg" style="background-size: 100% 100%;background-repeat:no-repeat" >
+
+<head>
+<p>contact us on www.hectecsolution.com<p></br>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+<title>
+Employee Management System|Home</title>
+<style> 
+	.footer{
+    position: fixed;
+	padding:10px;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: black;
+    color: white;
+    text-align: center;
+}
+</style>
+</head>
+
+      <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" >
+
+      <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css" >
+<script src="styles/bootstrap/js/jquery.min.js"></script>
+<script src="styles/bootstrap/js/bootstrap.min.js"></script>
+
+<div class="navbar-header" style="height:100px">
+<nav class="navbar navbar-inverse navbar-fixed-top" id="my-navbar">
+<div style="float:left">
+<a href="index.php" class="navbar-brand"><h2 style="color:white;margin-left:30px"><b>
+Employee Management System</b></h2></a></div>
+
+<div class="collapse navbar-collapse" id="navbar-collapse" style="float:right;margin-left:600px;margin-top:-40px;margin-right:20px">
+<h3>
+<ul class="nav navbar-nav">
+<li><a href="index.php">Home</a></li>
+<li><a href="login.php""#About us">Sign in</a></li>
+<li><a href="contactus.php">contact us</a></li>
+<li><a href="aboutus.php">About us</a></li>
+</ul></h3>
+
+</div>
+
+
+</nav>
+</div><BR><BR><BR><BR><BRs>
+
+
+   <div class="navbar navbar-default navbar-fixed-bottom">
+  <div class="container" style="color:red">
+    <p class="navbar-text">All Rights are reserved by Innovators &nbsp&nbsp<a href="index.php">www.inovators.com</a></p>
+	<form action="like.php" method="post">
+	</div>
+	<div class="footer">
+	<p><marquee direction="right">All Rights are reserved by Innovators</marquee></p>
+	</div>
+</body>
+</html>
